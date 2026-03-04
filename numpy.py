c951abb389bc156d76886fc9fca504ba3a2943cd
vidula1510-cell <vidulaeknathpatil1510@gmail.com>
2026-03-04 16:03:14 +0530

    Add files via upload

---
+++ numpy.py
@@ -0,0 +1,184 @@
+
+# coding: utf-8
+
+# In[2]:
+
+
+import numpy
+
+a = numpy.array(2)
+print(a)
+
+
+# In[3]:
+
+
+import numpy
+b = numpy.array([2,4,7,1])
+print(b)
+
+
+# In[21]:
+
+
+import numpy as np
+a = np.array(2,7,1,6)
+print(a)
+
+
+# In[12]:
+
+
+import numpy as np
+a = numpy.array(2,int)
+print(a)
+
+
+# In[11]:
+
+
+import numpy
+
+a = numpy.array(4,1)
+print(a)
+
+
+# In[15]:
+
+
+print(numpy.__version__)
+
+
+# In[16]:
+
+
+pip install numpy
+
+
+# In[17]:
+
+
+a = numpy.array(4)
+print(a)
+
+
+# In[18]:
+
+
+print(type(a))
+
+
+# In[19]:
+
+
+import numpy as ds
+abc = ds.array([2,5,7,5])
+print(abc)
+
+
+# In[20]:
+
+
+print(type(abc))
+
+
+# In[30]:
+
+
+a=numpy.array('5',int)
+print(a)
+
+
+# In[31]:
+
+
+a=numpy.array('5.9',float)
+print(a)
+
+
+# In[32]:
+
+
+b = numpy.array([2,4,5,6])
+print(b)
+
+
+# In[33]:
+
+
+b = numpy.array(['1','2',True,'a','10.10'])
+print(b)
+
+
+# In[34]:
+
+
+print(type(b))
+
+
+# In[38]:
+
+
+c = numpy.array(['-1','0.7','-1.9',1.9])
+print(c)
+
+
+# In[43]:
+
+
+import numpy as np1234567891vidulaeknathpatil
+a = np1234567891vidulaeknathpatil.array([9,6,8])
+print(a)
+
+
+# In[49]:
+
+
+a = numpy.array([1,'',3])
+print(a)
+
+
+# In[53]:
+
+
+m = numpy.array([])
+print(m)
+
+
+# In[51]:
+
+
+print(type(m))
+
+
+# In[54]:
+
+
+a = numpy.array((1,2,3,4))
+print(a)
+
+
+# In[55]:
+
+
+a1 = [2,5,4,2]
+a2 = (2,7,9,8)
+print('type of a1:',type(a1))
+print('type of a2:',type(a2))
+
+
+# In[56]:
+
+
+c1 = numpy.array(a1)
+print(c1)
+print(type(c1))
+
+
+# In[57]:
+
+
+g1 = numpy.array(a2)
+print(g1)
+print(type(g1))
+
